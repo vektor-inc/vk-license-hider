dab60e6f25cef645e8e39a586158cf96fe5aeb87
kurudrive <kurudrive@gmail.com>
2023-06-27 00:55:01 +0900

    first commit

---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== VK License Hider ===
+Contributors: vektor-inc,kurudrive,rickaddison7634
+Donate link:
+Tags: 
+Requires at least: 6.0
+Tested up to: 6.2
+Stable tag: 1.58.0.1
+Requires PHP: 7.2
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Hide License key form about Vektor products.
+
+== Description ==
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+e.g.
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Frequently Asked Questions ==
+
+
+== Screenshots ==
+
+
+
+== Changelog ==
+
+= 0.1.0 =
+First release
+
+== Upgrade Notice ==
+
+Nothing.
---
+++ vk-license-hider.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Plugin Name:     VK License Hider
+ * Plugin URI:      https://github.com/vektor-inc/vk-license-hider
+ * Description:
+ * Author:
+ * Author URI:
+ * Text Domain:     vk-license-hider
+ * Domain Path:     /languages
+ * Version:         0.0.0
+ * License:         GNU General Public License v2 or later
+ * License URI:     http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @package         vk-license-hider
+ */
+
+// Do not load directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * VK Blocks Pro Hide license key field.
+ *
+ * @param bool $display : Display or not.
+ * @return bool
+ */
+function vklh_vk_blocks_hide_license_key( $display ) {
+	$display = false;
+	return $display;
+}
+add_filter( 'vk_blocks_license_key_display_setting', 'vklh_vk_blocks_hide_license_key', 11 );
+
+
+/**
+ * G3 Pro Hide license key field.
+ *
+ * @param bool $display : Display or not.
+ * @return bool
+ */
+function vklh_g3_pro_hide_license_key( $display ) {
+	$display = false;
+	return $display;
+}
+add_filter( 'ltg3pro_license_key_display_setting', 'vklh_g3_pro_hide_license_key', 11 );
+
+
+
+
+
